bad6cfbadb791931eea359de91b5ee4863c9e344
Martin Di Paola <martinp.dipaola@gmail.com>
2021-01-16 17:37:41 +0000

    Refactor

--- xview/xview.py
+++ xview/xview.py
@@ -6,12 +6,170 @@ from struct import unpack
 '''
 
 
+class Ex:
+    def __init__(self, cols, fmt, unit, endianess, sep='  '):
+        self._validate_params(cols, fmt, unit, endianess, sep)
+        self._unpacker, self._sz = self._build_unpacker(fmt, unit, endianess)
+
+        self._formatter = self._build_formatter(fmt, unit, endianess, self._sz)
+
+        self._cols = cols
+        self._sep = sep
+
+    def _validate_params(self, cols, fmt, unit, endianess, sep):
+        if cols is not None and not (0 < cols <= 64):
+            raise ValueError(
+                "Column count '%s' must be a number between 1 and 64 or None."
+                % cols
+            )
+
+        if unit not in ('b', 'h', 'w', 'g'):
+            raise ValueError(
+                "Unit '%s' unknown. Expected (b)yte, (h)alf, (w)ord or (g)iant."
+                % unit
+            )
+
+        if fmt not in (
+            'x', 'd', 'u', 'o', 't', 'a', 'c', 'f', 's', 'z', 'r', 'i'
+        ):
+            raise ValueError(
+                "Format '%s' unknown. Expected he(x), signe(d), (u)nsigned, (o)ctal, (t)wo, (a)ddress, (c)har, (f)loat, (s)tring, (z)ero padded, (r)aw or (i)nstruction."
+                % fmt
+            )
+
+        if fmt == 'f' and unit not in ('w', 'g'):
+            raise ValueError(
+                "Format (f)loat can work only with (w)ords and (g)iants units but received '%s'"
+                % unit
+            )
+
+        if fmt in ('c', 's') and unit == 'g':
+            raise ValueError("Format (c)har/(s)tring can work (g)iant units.")
+
+        if fmt in ('a', 'i', 'z', 's'):
+            raise NotImplementedError("Format '%s' not supported yet." % fmt)
+
+        if endianess not in ('b', 'l', 'n'):
+            raise ValueError(
+                "Endianess type '%s' unknown. Expected (b)ig, (l)ittle or (n)ative."
+            )
+
+    def _build_unpacker(self, fmt, unit, endianess):
+        # format for struct.unpack
+        sfmt = {'b': '>', 'l': '<', 'n': '='}[endianess]
+
+        # struct.unpack opcode and size in bytes of the object
+        op, sz = {
+            'b': ('B', 1),
+            'h': ('H', 2),
+            'w': ('I', 4),
+            'g': ('L', 8),
+        }[unit]
+
+        if fmt == 'r':
+            op = '%is' % sz  # raw means raw string, no conversion
+
+        if fmt == 'd':
+            op = op.lower()  # signed version
+
+        if fmt == 'f':
+            assert sz in (4, 8)
+            op = 'f' if sz == 4 else 'd'
+
+        # encoding for string-like examinations based on the object's size
+        if fmt in ('c', 's'):
+            enc = {1: 'utf-8', 2: 'utf-16', 4: 'utf-32'}[sz]
+
+            force_big_endian = endianess == 'b' or (
+                endianess == 'n' and sys.byteorder == 'big'
+            )
+            if force_big_endian:
+                enc += '-be'
+            else:
+                assert endianess == 'l'
+                enc += '-le'
+
+        # final format for struct.unpack and method for unpacking;
+        # 'c' is special as it will not use struct.unpack at all
+        # but str
+        if fmt in ('c', ):
+            sfmt = None
+            unpack = partial(str, encoding=enc, errors='replace')
+        else:
+            sfmt = sfmt + op
+            unpack = partial(_unpack_1, sfmt=sfmt)
+
+        return unpack, sz
+
+    def _build_formatter(self, fmt, unit, endianess, sz):
+        # representation type (method format()) and padding per byte
+        ftype, npad = {
+            'x': ('x', 2),
+            'd': ('d', 0),
+            'u': ('d', 0),  # same as 'd'
+            'o': ('o', 3),
+            't': ('b', 8),
+            'f': ('e', 0),
+            'c': ('', 0),
+            'r': ('', 0),
+            's': ('', 0),
+        }[fmt]
+
+        # eg: an he(x) with npad of 2 requires 8 bytes of padding
+        # to print a 4 bytes numbers like 0x0000abcd
+        npad *= sz
+
+        # representation format (method format())
+        # eg: {0:08x} or {0:d} or {0:}
+        if npad:
+            rfmt = '{0:0{npad}{ftype}}'
+        else:
+            rfmt = '{0:{ftype}}'
+
+        return partial(_format_1, rfmt=rfmt, npad=npad, ftype=ftype)
+
+    def print(self, mem):
+        mem = memoryview(mem)  # avoid copies when slicing
+        cols = self._cols
+        sep = self._sep
+
+        sz = self._sz
+        unpack = self._unpacker
+        formatter = self._formatter
+
+        offset = 0
+        cnt = len(mem) // sz
+        line = []
+        for i, offset in enumerate(range(0, cnt * sz, sz), 1):
+            m = mem[offset:offset + sz]
+
+            obj = unpack(m)
+
+            line.append(formatter(obj))
+
+            if cols is None:
+                assert len(line) == 1
+                print(*line, end=sep)
+                line.clear()
+
+            elif i % cols == 0:
+                print(*line, sep=sep)
+                line.clear()
+
+        if line:
+            print(*line, sep=sep)
+
+
 def _unpack_1(data, sfmt):
     # note: the colon after 'obj' forces a tuple unpack. If
     # the method returns anything else except a tuple with a single element
     # this will fail (and it should it!)
     obj, = unpack(sfmt, data)
     return obj
+
+
+def _format_1(obj, rfmt, **kargs):
+    return rfmt.format(obj, **kargs)
 
 
 # https://sourceware.org/gdb/current/onlinedocs/gdb/Memory.html
@@ -110,139 +268,4 @@ def examine(mem, cols, fmt, unit, endianess, sep='  '):
         7.160907e-313
 
         '''
-    if cols is not None and not (0 < cols <= 64):
-        raise ValueError(
-            "Column count '%s' must be a number between 1 and 64 or None." %
-            cols
-        )
-
-    if unit not in ('b', 'h', 'w', 'g'):
-        raise ValueError(
-            "Unit '%s' unknown. Expected (b)yte, (h)alf, (w)ord or (g)iant." %
-            unit
-        )
-
-    if fmt not in ('x', 'd', 'u', 'o', 't', 'a', 'c', 'f', 's', 'z', 'r', 'i'):
-        raise ValueError(
-            "Format '%s' unknown. Expected he(x), signe(d), (u)nsigned, (o)ctal, (t)wo, (a)ddress, (c)har, (f)loat, (s)tring, (z)ero padded, (r)aw or (i)nstruction."
-            % fmt
-        )
-
-    if fmt == 'f' and unit not in ('w', 'g'):
-        raise ValueError(
-            "Format (f)loat can work only with (w)ords and (g)iants units but received '%s'"
-            % unit
-        )
-
-    if fmt in ('c', 's') and unit == 'g':
-        raise ValueError("Format (c)har/(s)tring can work (g)iant units.")
-
-    if fmt in ('a', 'i', 'z', 's'):
-        raise NotImplementedError("Format '%s' not supported yet." % fmt)
-
-    if endianess not in ('b', 'l', 'n'):
-        raise ValueError(
-            "Endianess type '%s' unknown. Expected (b)ig, (l)ittle or (n)ative."
-        )
-
-    mem = memoryview(mem)  # avoid copies when slicing
-
-    # format for struct.unpack
-    sfmt = {'b': '>', 'l': '<', 'n': '='}[endianess]
-
-    # struct.unpack opcode and size in bytes of the object
-    op, sz = {
-        'b': ('B', 1),
-        'h': ('H', 2),
-        'w': ('I', 4),
-        'g': ('L', 8),
-    }[unit]
-
-    if fmt == 'r':
-        op = '%is' % sz  # raw means raw string, no conversion
-
-    if fmt == 'd':
-        op = op.lower()  # signed version
-
-    if fmt == 'f':
-        assert sz in (4, 8)
-        op = 'f' if sz == 4 else 'd'
-
-    # encoding for string-like examinations based on the object's size
-    if fmt in ('c', 's'):
-        enc = {1: 'utf-8', 2: 'utf-16', 4: 'utf-32'}[sz]
-
-        force_big_endian = endianess == 'b' or (
-            endianess == 'n' and sys.byteorder == 'big'
-        )
-        if force_big_endian:
-            enc += '-be'
-        else:
-            assert endianess == 'l'
-            enc += '-le'
-
-    # final format for struct.unpack and method for unpacking;
-    # 'c' is special as it will not use struct.unpack at all
-    # but str
-    if fmt in ('c', ):
-        sfmt = None
-        unpack = partial(str, encoding=enc, errors='replace')
-    else:
-        sfmt = sfmt + op
-        unpack = partial(_unpack_1, sfmt=sfmt)
-
-    # not needed anymore
-    del sfmt
-
-    # TODO not supported yet
-    #if fmt == 's':
-    #    print(repr(''.join(codecs.iterdecode([mem], 'utf-16-le', 'ignore'))))
-    #    return
-
-    assert fmt != 's'
-
-    # representation type (method format()) and padding per byte
-    ftype, npad = {
-        'x': ('x', 2),
-        'd': ('d', 0),
-        'u': ('d', 0),  # same as 'd'
-        'o': ('o', 3),
-        't': ('b', 8),
-        'f': ('e', 0),
-        'c': ('', 0),
-        'r': ('', 0),
-        's': ('', 0),
-    }[fmt]
-
-    # eg: an he(x) with npad of 2 requires 8 bytes of padding
-    # to print a 4 bytes numbers like 0x0000abcd
-    npad *= sz
-
-    # representation format (method format())
-    # eg: {0:08x} or {0:d} or {0:}
-    if npad:
-        rfmt = '{0:0{npad}{ftype}}'
-    else:
-        rfmt = '{0:{ftype}}'
-
-    offset = 0
-    cnt = len(mem) // sz
-    line = []
-    for i, offset in enumerate(range(0, cnt * sz, sz), 1):
-        m = mem[offset:offset + sz]
-
-        obj = unpack(m)
-
-        line.append(rfmt.format(obj, npad=npad, ftype=ftype))
-
-        if cols is None:
-            assert len(line) == 1
-            print(*line, end=sep)
-            line.clear()
-
-        elif i % cols == 0:
-            print(*line, sep=sep)
-            line.clear()
-
-    if line:
-        print(*line, sep=sep)
+    return Ex(cols, fmt, unit, endianess, sep).print(mem)
